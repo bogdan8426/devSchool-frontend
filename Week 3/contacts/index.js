0d45b74231a4707c57adbdeb7b19b3110b8d60ec
Bogdan Rotaru <rotarubogdan96@gmail.com>
2020-04-16 13:04:55 +0300

    Enable/Disable delete button

--- Week 3/contacts/index.js
+++ Week 3/contacts/index.js
@@ -1,64 +1,78 @@
-import {append, read, deleteContactByIndex} from './storage.js';
+import { append, read, deleteContactByIndex } from './storage.js';
 
-export function init(){
-    window.addEventListener('DOMContentLoaded', onLoad);
+export function init() {
+  window.addEventListener('DOMContentLoaded', onLoad);
 }
 
-function onLoad(event){
-
-
-    document.getElementById('form-add')
-        .addEventListener('submit', onSubmitAdd);
-        
-    document.getElementById('form-delete')
-        .addEventListener('submit', onSubmitDelete);
-
-    render();
-        
+function onLoad() {
+  document.getElementById('form-add').addEventListener('submit', onSubmitAdd);
+  document.getElementById('form-delete').addEventListener('submit', onSubmitDelete);
+  document.getElementById('list').addEventListener('click', checkboxChanged);
+  render();
 }
 
-function onSubmitAdd(event){
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const contact = Object.fromEntries(formData);
+function onSubmitAdd(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const contact = Object.fromEntries(formData);
 
-    append(contact);
-    render();
+  append(contact);
+  render();
 }
 
-function onSubmitDelete(event){
-    event.preventDefault();
-    
-    let indexArray = [];
-    document.getElementsByName("delete").forEach(element => {
-        if(element.checked){
-            const li = element.closest('li');
-            const liNodes = Array.from( li.closest('ul').children );
-            const index = liNodes.indexOf(li);
-            indexArray.push(index);
-        }
-    });
+function onSubmitDelete(event) {
+  event.preventDefault();
 
-    while(indexArray.length > 0){
-        deleteContactByIndex(indexArray.pop());
+  let indexArray = [];
+  document.getElementsByName('delete').forEach(element => {
+    if (element.checked) {
+      const li = element.closest('li');
+      const liNodes = Array.from(li.closest('ul').children);
+      const index = liNodes.indexOf(li);
+      indexArray.push(index);
     }
+  });
 
-    render();
+  while (indexArray.length > 0) {
+    deleteContactByIndex(indexArray.pop());
+  }
 
+  render();
 }
 
-function render(){
-    const contacts = read();
-    const list = document.getElementById('list');
+function render() {
+  const contacts = read();
+  const list = document.getElementById('list');
 
-    const items = contacts.map(contact => `
+  const items = contacts.map(
+    contact => `
         <li>
             <input type="checkbox" name="delete"/>
             ${contact.name} &lt;${contact.email}&gt; (${contact.phone}) 
         </li>
-    `);
+    `
+  );
 
-    list.innerHTML = items.join('');
+  list.innerHTML = items.join('');
+  const deleteButton = document.getElementById('delete-button');
+  deleteButton.disabled = true;
+  deleteButton.hidden = contacts.length === 0;
+}
+
+function checkboxChanged(event) {
+  const checkbox = event.target;
+  if (checkbox.type === 'checkbox') {
     const deleteButton = document.getElementById('delete-button');
-    deleteButton.hidden = contacts.length === 0;
+    
+    if (checkbox.checked) {
+      deleteButton.disabled = false;
+    } else {
+      const liElements = document.getElementsByName('delete');
+      for (let element of liElements) {
+        if (element.checked) return;
+      }
+      // No checked elements
+      deleteButton.disabled = true;
+    }
+  }
 }
--- Week 3/contacts/storage.js
+++ Week 3/contacts/storage.js
@@ -1,26 +1,25 @@
-export function read(){
-    const data = window.localStorage.getItem('ds-contacts');
-    return data === null ? [] : JSON.parse(data);
+export function read() {
+  const data = window.localStorage.getItem('ds-contacts');
+  return data === null ? [] : JSON.parse(data);
 }
 
-function write(contacts){
-    const data = JSON.stringify(contacts);
-    window.localStorage.setItem('ds-contacts', data);
+function write(contacts) {
+  const data = JSON.stringify(contacts);
+  window.localStorage.setItem('ds-contacts', data);
 }
 
-export function append(contact){
+export function append(contact) {
+  const contacts = read();
+
+  contacts.push(contact);
+
+  write(contacts);
+}
+
+export function deleteContactByIndex(index) {
+  if (index > -1) {
     const contacts = read();
-
-    contacts.push(contact);
-
+    contacts.splice(index, 1);
     write(contacts);
-}
-
-export function deleteContactByIndex(index){
-    
-    if(index>-1){
-        const contacts = read();
-        contacts.splice(index, 1);
-        write(contacts);
-    }
+  }
 }
